a57d34eaffba5e85d863e6253c1789f855de75c3
miljovska <ammiljovska@gmail.com>
2020-04-12 21:36:28 +0200

    taxReturn method finished

--- Item.java
+++ Item.java
@@ -13,8 +13,47 @@ class Item {
 		this.vid_danok = vid_danok;
 	}
 	//TODO setters and getters
+	public int getId() {
+		return id;
+	}
 
-	double taxReturn () {
-		//TODO
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getIme() {
+		return ime;
+	}
+
+	public void setIme(String ime) {
+		this.ime = ime;
+	}
+
+	public double getCena() {
+		return cena;
+	}
+
+	public void setCena(double cena) {
+		this.cena = cena;
+	}
+
+	public char getVid_danok() {
+		return vid_danok;
+	}
+
+	public void setVid_danok(char vid_danok) {
+		this.vid_danok = vid_danok;
+	}
+		double taxReturn (int id,int cena,char vid_danok) {
+		double taxr;
+		if(vid_danok=='A')
+		taxr=float(cena*18*15)/10000
+		elseif(vid_danok=='B')
+		taxr=float(cena*5*15)/10000
+		else
+		taxr=float(cena*15)/10000
+	}
+	public double getTaxRetrun(){
+		return taxr;
 	}
 }
